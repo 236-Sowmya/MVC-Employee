40b6e29e3a36ae9441265d2ab26e1cf550c23585
Sai Ganga <Sai Ganga@DESKTOP-M8QD404>
2021-12-31 11:35:08 +0530

    Add project files.

---
+++ MVC-Employee/Controllers/StudentController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_Employee.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult StduentId()
+        {
+            return View();
+        }
+        public IActionResult StudentName()
+        {
+            return View();
+        }
+    }
+}
